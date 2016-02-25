e93cca56051adfe4542e93243b325cd8ccfad71d
Jinyoung Eom <jinyoung.eom@gmail.com>
2016-02-25 20:50:51 +0900

    .

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -4,8 +4,18 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("홍길동");
+		
+		if (10 < 12) 
+			System.out.println("....");
+		
+		switch(10) {
+		case 10: break;
+		default:
+		}
+		
+		System.out.println("용근쓰");
 		System.out.println("아아");
 
-		System.out.println("용근쓰");
 	}
+
 }

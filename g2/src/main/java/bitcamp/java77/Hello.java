a1238ef0db1fd46dd52564d9618ecf9444dae263
cocoball28 <jjinoya@hotmail.com>
2016-02-25 20:58:14 +0900

    .

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("홍길동");
-		
+
 		if (10 < 12) 
 			System.out.println("....");
 		
@@ -16,8 +16,10 @@ public class Hello {
 			System.out.println("ㅋㅋ");
 		}
 		System.out.println("용근쓰");
+		
+		String aa = "error";
 		System.out.println("아아");
-
+		System.out.println("용근쓰");
 	}
 
 }
